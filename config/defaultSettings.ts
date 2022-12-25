15514face87d48f360f26f94b6dd48b375f05606
mailinhtvtcna <mailinhtvtcna@gmail.com>
2022-12-25 20:44:47 +0700

    feat: change logo

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -14,7 +14,7 @@ const Settings: LayoutSettings & {
   colorWeak: false,
   title: 'Linh Restaurant',
   pwa: false,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: '/logo_girl_chef.png',
   iconfontUrl: '',
 };
 
